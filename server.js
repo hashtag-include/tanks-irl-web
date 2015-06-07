821b750fe0bb746ebe28f7c679e0dcfabd89ebdb
Trevor Stevens <trevor@sociallytied.com>
2015-06-07 15:32:39 -0400

    Adding process.env.PORt for azure instead of hardcoding

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-app.listen(1337);
+app.listen(process.env.PORT || 1337);
 
 function handler (req, res) {
     fs.readFile(__dirname + '/index.html',
