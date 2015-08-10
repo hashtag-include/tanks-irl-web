b81c4f3a680f4887fe90befbf0d2ad2b968280a4
Trevor Stevens <trevor@sociallytied.com>
2015-08-09 22:43:24 -0400

    Cleared hardcoded gamelist

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static('public'));
 
-var gameList = { sessions: [], unmatched: ["5208", "9102", "2138"] };
+var gameList = { sessions: [], unmatched: [] };
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
